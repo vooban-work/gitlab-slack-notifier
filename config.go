0874e7041c8a6016ed2ca147e834948226a1484a
Jose Ouellet <jose.ouellet@vooban.com>
2026-05-20 09:56:35 -0400

    ping check

--- config.go
+++ config.go
@@ -15,3 +15,6 @@ var GITLAB_WEBHOOK_SECRET_TOKEN string = os.Getenv("GITLAB_WEBHOOK_SECRET_TOKEN"
 var SLACK_EVENT_READ_CHANNEL string = os.Getenv("SLACK_EVENT_READ_CHANNEL")   // optional, monitor one channel "CH1234567"
 var USER_EMAIL_DOMAIN string = os.Getenv("USER_EMAIL_DOMAIN")                 // required, email domain, "business.com"
 var USER_EMAIL_SPACE_REPLACER string = os.Getenv("USER_EMAIL_SPACE_REPLACER") // optional, leave empty to replace spaces to nothing, "."
+
+var PING_PONG_URL string = os.Getenv("PING_PONG_URL")                        // required for keep-alive, public URL hit on interval, "https://gitlabrador.onrender.com/healthcheck"
+var PING_PONG_INTERVAL_MINUTES int = getEnvInt("PING_PONG_INTERVAL_MINUTES") // optional, interval in minutes for keep-alive ping-pong, default 1
